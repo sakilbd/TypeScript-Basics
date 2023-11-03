6920bc17db3917d7608737a7b5c5e24df56dba76
unknown <mdsakiluzzaman@gmail.com>
2023-11-03 20:39:05 +0600

    module 2 started

---
+++ module2/2.1.ts
@@ -0,0 +1,37 @@
+{
+    //type assertion 
+
+    let anything: any;
+
+    anything = "Next Level Web developement";
+    anything = 223;
+
+    (anything as number).
+
+
+    const kgToGm = (value: string | number): string | number | undefined => {
+        if (typeof value === 'string') {
+            const convertedValue = parseFloat(value) * 1000
+            return `the converted value is ${convertedValue}`;
+        }
+        if (typeof value === 'number') {
+            return value * 1000;
+        }
+    }
+
+    const result1 = kgToGm(1000) as number
+    const result = kgToGm("1000") as string
+
+    type customError = {
+        message: string
+    }
+
+    try {
+
+    }
+    catch (error) {
+        console.log((error as customError).message)
+    }
+
+
+}
